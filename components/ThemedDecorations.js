df8ade31b25281ebaf120645619331c9d486c251
cy2003 <catyyee@hotmail.com>
2017-02-01 16:12:47 -0500

    done

---
+++ components/ThemedDecorations.js
@@ -0,0 +1,22 @@
+const React = require('react');
+
+class ThemedDecorations extends React.Component {
+  render(){
+
+    const newChildren = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      })
+    })
+    return (
+      <div>
+        {newChildren}
+      </div>
+    )
+
+  }
+}
+
+
+
+module.exports = ThemedDecorations
